1493844bb3298e0a90e9bc3fdece9c9c2b9618ac
da286448 <dtrujillo.rod@gmail.com>
2016-12-22 11:00:16 -0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,15 +1,50 @@
-setwd("/Users/Dtrujillo/RCourse")
-Data <- read.table("household_power_consumption.txt",header = TRUE, sep = ";",, na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
-Data$Date <- as.Date(Data$Date, "%d/%m/%Y")
-dateTime <- paste(Data$Date, Data$Time)
 
-png(filename='plot2.png', width=480, height=480, units='px')
+requiredPackages <- c("sqldf", "utils")
+ipak <- function(pkg)
+{
+        new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+        if (length(new.pkg))
+                install.packages(new.pkg, dependencies = TRUE)
+        sapply(pkg, require, character.only = TRUE)
+}
+ipak(requiredPackages)
 
-startDay = as.POSIXct(strftime("2007-02-01 00:00:00"))
-endDay = as.POSIXct(strftime("2007-02-03 00:00:00"))
-plot(Data$Global_active_power~Data$Date, type="l", ylab="Global Active Power (kilowatts)", xlab="")
-     ylab="Global_active_power"
-)
-axis.POSIXct(1, at=seq(startDay, endDay, by="day"), format="%a")
+# invoke libraries
+library(utils)
+library(sqldf)
 
-dev.off()
+## downnload the source file from Internet
+if(!file.exists("household_power_consumption.txt")) {
+        temp <- tempfile()
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+        unlink(temp)
+}
+
+## read household power consumption data (constraining on first two days of Feb 2007)
+power_data <- read.csv.sql("household_power_consumption.txt", 
+                           sql = " SELECT * FROM file 
+                                   WHERE Date = '1/2/2007' 
+                                      OR Date = '2/2/2007'", 
+                           sep=';', header=TRUE)
+
+## format date and transform the power variables
+power_data$Date <- as.Date(power_data$Date, format = "%d/%m/%Y")
+power_df <- power_data[(power_data$Date == "2007-02-01") | (power_data$Date == "2007-02-02"),]
+power_df$Global_active_power <- as.numeric(as.character(power_df$Global_active_power))
+power_df$Global_reactive_power <- as.numeric(as.character(power_df$Global_reactive_power))
+power_df$Voltage <- as.numeric(as.character(power_df$Voltage))
+power_df <- transform(power_df, timestamp = as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+power_df$Sub_metering_1 <- as.numeric(as.character(power_df$Sub_metering_1))
+power_df$Sub_metering_2 <- as.numeric(as.character(power_df$Sub_metering_2))
+power_df$Sub_metering_3 <- as.numeric(as.character(power_df$Sub_metering_3))
+
+## reproduce plot 2
+plot2 <- function() {
+        plot(power_df$timestamp,power_df$Global_active_power, 
+             type = "l",                 ## create line type
+             xlab = "",                  ## with no X-axis label
+             ylab = "Global Active Power (kilowatts)")
+        dev.copy(png, file = "plot2.png", width = 480, height = 480)
+        dev.off()
+}
+plot2()
