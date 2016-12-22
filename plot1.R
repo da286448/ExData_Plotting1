af44590a414d0907369eebe0e74194296162a712
da286448 <dtrujillo.rod@gmail.com>
2016-12-22 10:59:02 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,8 +1,50 @@
-dataFile <- "./data/week1_expdata/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.off()
+requiredPackages <- c("sqldf", "utils")
+ipak <- function(pkg)
+{
+        new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+        if (length(new.pkg))
+                install.packages(new.pkg, dependencies = TRUE)
+        sapply(pkg, require, character.only = TRUE)
+}
+ipak(requiredPackages)
+
+
+library(utils)
+library(sqldf)
+
+
+if(!file.exists("household_power_consumption.txt")) {
+        temp <- tempfile()
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+        unlink(temp)
+}
+
+## read household power consumption data (constraining on first two days of Feb 2007)
+power_data <- read.csv.sql("household_power_consumption.txt", 
+                           sql = " SELECT * FROM file 
+                                   WHERE Date = '1/2/2007' 
+                                      OR Date = '2/2/2007'", 
+                           sep=';', header=TRUE)
+
+## format date and transform the power variables
+power_data$Date <- as.Date(power_data$Date, format = "%d/%m/%Y")
+power_df <- power_data[(power_data$Date == "2007-02-01") | (power_data$Date == "2007-02-02"),]
+power_df$Global_active_power <- as.numeric(as.character(power_df$Global_active_power))
+power_df$Global_reactive_power <- as.numeric(as.character(power_df$Global_reactive_power))
+power_df$Voltage <- as.numeric(as.character(power_df$Voltage))
+power_df <- transform(power_df, timestamp = as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+power_df$Sub_metering_1 <- as.numeric(as.character(power_df$Sub_metering_1))
+power_df$Sub_metering_2 <- as.numeric(as.character(power_df$Sub_metering_2))
+power_df$Sub_metering_3 <- as.numeric(as.character(power_df$Sub_metering_3))
+
+## reproduce plot 1
+plot1 <- function() {
+        hist(power_df$Global_active_power, 
+             main = "Global Active Power", 
+             col = "red", 
+             xlab = "Global Active Power (kilowatts)")
+        dev.copy(png, file = "plot1.png", width = 480, height = 480)
+        dev.off()
+}
+plot1()
